4e9f11ee2f630e18804c62a979ad87a79cb12c03
danielmartensson <daniel.martensson100@outlook.com>
2021-07-20 16:49:01 +0200

    Added Read Commanded Address function here

--- Src/SAE J1939/SAE J1939-81 Network Management Layer/Commanded_Address.c
+++ Src/SAE J1939/SAE J1939-81 Network Management Layer/Commanded_Address.c
@@ -9,7 +9,7 @@
 
 /*
  * Send commanded address to another ECU
- * PGN 0x00FED8 (65240)
+ * PGN: 0x00FED8 (65240)
  */
 ENUM_J1939_STATUS_CODES SAE_J1939_Send_Commanded_Address(J1939 *j1939, uint8_t DA, uint8_t new_ECU_address, uint32_t identity_number, uint16_t manufacturer_code, uint8_t function_instance, uint8_t ECU_instance, uint8_t function, uint8_t vehicle_system, uint8_t arbitrary_address_capable, uint8_t industry_group, uint8_t vehicle_system_instance) {
 	/* Multiple messages - Use Transport Protocol Connection Management BAM */
@@ -30,12 +30,27 @@ ENUM_J1939_STATUS_CODES SAE_J1939_Send_Commanded_Address(J1939 *j1939, uint8_t D
 		return status;
 	status = SAE_J1939_Send_Transport_Protocol_Data_Transfer(j1939, DA, data, total_message_size, number_of_packages);
 	if(status == STATUS_SEND_OK){
-		/* Delete old destination address */
-		j1939->ECU_address[DA] = 0;
-		// TODO: Find a PGN request that can delete a ECU address from a network
+		j1939->ECU_address[DA] = 0; 						/* Delete old destination address */
+		// TODO: Find a PGN request that can j1939->ECU_address[DA] = 0 for all ECU
 		/* According to J1939 standard, request for Address Claimed (broadcast) must be sent after a Commanded Address so we can get back the new name and address */
 		SAE_J1939_Send_Request(j1939, new_ECU_address, PGN_ADDRESS_CLAIMED);
 	}
 	return status;
 }
 
+/*
+ * Read the commanded address from another ECU. Will always be called from Transport Protocol Data Transfer due to 9 bytes of data
+ * PGN: 0x00FED8 (65240)
+ */
+void SAE_J1939_Read_Commanded_Address(J1939 *j1939, uint8_t data[]) {
+	j1939->this_name.identity_number = ((data[2] & 0b00011111) << 16) | (data[1] << 8) | data[0];
+	j1939->this_name.manufacturer_code = (data[3] << 3) | (data[2] >> 5);
+	j1939->this_name.function_instance = data[4] >> 3;
+	j1939->this_name.ECU_instance = data[4] & 0b00000111;
+	j1939->this_name.function = data[5];
+	j1939->this_name.vehicle_system = data[6] >> 1;
+	j1939->this_name.arbitrary_address_capable = data[7] >> 7;
+	j1939->this_name.industry_group = (data[7] >> 4) & 0b0111;
+	j1939->this_name.vehicle_system_instance = data[7] & 0b00001111;
+	j1939->this_ECU_address = data[8]; 						/* New address of this ECU */
+}
