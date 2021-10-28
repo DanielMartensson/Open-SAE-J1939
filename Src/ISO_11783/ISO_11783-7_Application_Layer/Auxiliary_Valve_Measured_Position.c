22a3d1b7fa1bfee70fddff46116dcf442e35c8cb
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-10-28 11:14:41 +0200

    Correct valve state now for measured valve position

--- Src/ISO_11783/ISO_11783-7_Application_Layer/Auxiliary_Valve_Measured_Position.c
+++ Src/ISO_11783/ISO_11783-7_Application_Layer/Auxiliary_Valve_Measured_Position.c
@@ -28,7 +28,7 @@ ENUM_J1939_STATUS_CODES ISO_11783_Response_Request_Auxiliary_Valve_Measured_Posi
 	uint8_t data[8];
 	data[0] = j1939->this_auxiliary_valve_measured_position[valve_number].measured_position_percent;
 	data[1] = j1939->this_auxiliary_valve_measured_position[valve_number].measured_position_percent >> 8;
-	data[2] = j1939->this_auxiliary_valve_measured_position[valve_number].valve_state;
+	data[2] = 0b11110000 | j1939->this_auxiliary_valve_measured_position[valve_number].valve_state;
 	data[3] = j1939->this_auxiliary_valve_measured_position[valve_number].measured_position_micrometer;
 	data[4] = j1939->this_auxiliary_valve_measured_position[valve_number].measured_position_micrometer >> 8;
 	data[5] = data[6] = data[7] = 0xFF;								/* All reserved */
@@ -41,7 +41,7 @@ ENUM_J1939_STATUS_CODES ISO_11783_Response_Request_Auxiliary_Valve_Measured_Posi
  */
 void ISO_11783_Read_Response_Request_Auxiliary_Valve_Measured_Position(J1939 *j1939, uint8_t SA, uint8_t valve_number, uint8_t data[]) {
 	j1939->from_other_ecu_auxiliary_valve_measured_position[valve_number].measured_position_percent = (data[1] << 8) | data[0];
-	j1939->from_other_ecu_auxiliary_valve_measured_position[valve_number].valve_state = data[2];
+	j1939->from_other_ecu_auxiliary_valve_measured_position[valve_number].valve_state = 0b00001111 & data[2];
 	j1939->from_other_ecu_auxiliary_valve_measured_position[valve_number].measured_position_micrometer = (data[4] << 8) | data[3];
 	j1939->from_other_ecu_auxiliary_valve_measured_position[valve_number].from_ecu_address = SA;
 }
