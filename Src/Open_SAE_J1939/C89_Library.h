be6bcd56aae68d62ad4d9e590bdbd769a5bf0a6f
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-07-25 11:45:43 +0200

    remove size_t and ssize_t

--- Src/Open_SAE_J1939/C89_Library.h
+++ Src/Open_SAE_J1939/C89_Library.h
@@ -22,11 +22,9 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long size_t;
 typedef signed char int8_t;
 typedef signed short int16_t;
 typedef signed int int32_t;
-typedef signed long ssize_t;
 
 /* Standard bool */
 typedef uint8_t bool;
