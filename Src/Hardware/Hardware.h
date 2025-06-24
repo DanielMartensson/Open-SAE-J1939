1f3143323e05ec049135cf06f5d3b97a24773962
Rickard Hallerback <rickard.hallerback@gmail.com>
2025-06-24 12:43:02 +0200

    Make PROCESSOR_CHOICE settable via -D flags during build

--- Src/Hardware/Hardware.h
+++ Src/Hardware/Hardware.h
@@ -16,7 +16,9 @@
 #define AVR 4
 #define QT_USB 5
 #define INTERNAL_CALLBACK 6
+#ifndef PROCESSOR_CHOICE
 #define PROCESSOR_CHOICE NO_PROCESSOR
+#endif
 
 /* C Standard library */
 #include "../Open_SAE_J1939/C89_Library.h"
