504b1dba041819d9a693d2e2b6e497ed9a1acdb1
Rickard Hallerback <rickard.hallerback@gmail.com>
2025-07-16 10:30:48 +0200

    Minor cleanup based on review

--- Src/SAE_J1939/SAE_J1939-71_Application_Layer/Application_Layer.c
+++ Src/SAE_J1939/SAE_J1939-71_Application_Layer/Application_Layer.c
@@ -5,7 +5,7 @@ extern "C" {
 #include "Application_Layer.h"
 
 /* Callback functions */
-void (*Callback_Function_Application)(SAE_Application_Info) = 0;
+void (*Callback_Function_Application)(SAE_Application_Info) = NULL;
 
 void SAE_J1939_Set_Application_Callback_Function(
     void (*Callback_Function)(SAE_Application_Info)
